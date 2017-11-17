389d3c02ec119f709039eea43c5c5fd7f4cfc083
Mark Geller <markg@condukia.com>
2017-11-17 23:11:09 +0000

    Learning about ejs

---
+++ MoreExpress/EJSDemo/app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+
+
+// ==================== ROUTES ====================
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+   var thing = req.params.thing;
+   res.render("love.ejs", {
+       thingVar: thing
+   });
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [
+            {title: "Post 1", author: "Susy"},
+            {title: "My adorable pet bunny", author: "Charlie"},
+            {title: "Can you believe this pomsky?", author: "Mark"}
+       ];
+    res.render("posts.ejs", {posts: posts});
+});
+
+
+// ==================== SERVER ====================
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is listening"); 
+});
