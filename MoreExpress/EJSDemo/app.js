2530f93874df83412a2e87b134e552045e98022f
Mark Geller <markg@condukia.com>
2017-12-27 23:21:59 +0000

    added partials

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 
 // ==================== ROUTES ====================
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
    var thing = req.params.thing;
-   res.render("love.ejs", {
+   res.render("love", {
        thingVar: thing
    });
 });
@@ -20,7 +23,7 @@ app.get("/posts", function(req, res) {
             {title: "My adorable pet bunny", author: "Charlie"},
             {title: "Can you believe this pomsky?", author: "Mark"}
        ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 
